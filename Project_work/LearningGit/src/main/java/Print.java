34c3e23236fb38b3a111735b7ecc9159e5c64ce1
GoRicha <100351509+GoRicha@users.noreply.github.com>
2022-02-24 16:59:29 +0530

    Update Print.java
    added more changes

--- Project_work/LearningGit/src/main/java/Print.java
+++ Project_work/LearningGit/src/main/java/Print.java
@@ -1,6 +1,6 @@
 public class Print {
     public static void main(String[] args) {
-        System.out.println("Hi i am print class - goricha leraning");
+        System.out.println("Hi i am print class - goricha leraning -123");
         //trying to learn pull request
     }
 }
