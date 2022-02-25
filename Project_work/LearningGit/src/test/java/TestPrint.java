41e804efe219cf914d9bf91c5a7c70df178c96ea
Richa2198 <richamahobia21@gmail.com>
2022-02-25 12:23:50 +0530

    commit 2

--- Project_work/LearningGit/src/test/java/TestPrint.java
+++ Project_work/LearningGit/src/test/java/TestPrint.java
@@ -2,5 +2,13 @@ public class TestPrint {
     public static void main(String[] args) {
         System.out.println("I am test class");
         //Neeraj added this
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
     }
 }
